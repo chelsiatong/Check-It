91e9135acc098f3e1dda17e887fc32307edeaa41
chelsia tong <chelsiatong@gmail.com>
2022-07-26 19:25:17 -0400

    new branch made

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import './App.css';
+// import Header from './components/header/header';
+// import Home from './pages/home';
+// import Notes from './components/notes/notes';
+// import Calendar from './components/calendar/calendar';
+// import About from './components/about/about';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
-    <div>
-      <h1>hello!</h1>
-    </div>
+    <Router>
+      {/* <Header /> */}
+      <Switch>
+        {/* <Route path='/' exact component={Home} /> */}
+        {/* <Route path='/notes' component={Notes} />
+        <Route path='/calendar' component={Calendar} />
+        <Route path='/about' component={About} /> */}
+      </Switch>
+    </Router>
+
   );
 }
 
---
+++ src/pages/home.js
@@ -0,0 +1,13 @@
+// import './home.scss';
+
+
+// function Home() {
+//     return (
+//         <div>
+//             hello!
+//         </div>
+
+//     );
+// }
+
+// export default Home;
