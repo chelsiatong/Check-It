ad126c639e545c86d8764dd5fe54f2d4312995b4
chelsia tong <chelsiatong@gmail.com>
2022-07-27 18:35:05 -0400

    styling notes and structuring react

--- src/components/mainnotes/mainnotes.js
+++ src/components/mainnotes/mainnotes.js
@@ -3,7 +3,18 @@ import './mainnotes.scss';
 
 function MainNotes() {
     return (
-        <p>main notes</p>
+        <section className="main-notes">
+            <div className="main-notes--edit" >
+                <input className="main-notes--title" type="text" id="title" placeholder="What do you want to name me?" autoFocus />
+                <textarea className="main-notes--body" id="body" placeholder="What's on your mind?" />
+            </div>
+            <div className="main-notes__preview" >
+                <h2 className="main-notes__preview--title" >Title</h2>
+                <div className="main-notes__preview--body" >Note Preview</div>
+            </div>
+
+        </section>
+
     )
 
 }
--- src/components/sidenotes/sidenotes.js
+++ src/components/sidenotes/sidenotes.js
@@ -5,14 +5,14 @@ function SideNotes() {
     return (
         <section className="side-notes">
             <div className="side-notes__header">
-                <button className="side-notes__add" >+ sdfgsfgsasdfadsfadsfadffd</button>
+                <button className="side-notes__add" >+</button>
                 <div className="side-notes__notes" >
                     <div className="side-notes__note" >
                         <div className="side-notes__title">
-                            <h4>Title</h4>
-                            <button className="side-notes__delete" >-</button>
+                            <h4 className="side-notes__label" >Title</h4>
+                            <button className="side-notes__delete">delete</button>
                         </div>
-                        <h5>note preview</h5>
+                        <h5 className="side-notes__preview" >note preview</h5>
                         <p className="side-notes__time">last modified time</p>
                     </div>
                 </div>
