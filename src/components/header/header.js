b855b22517b3315f6d714c446fdc060e4512d371
chelsia tong <chelsiatong@gmail.com>
2022-07-27 12:44:24 -0400

    trying to add react-scroll functionality

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,15 +1,19 @@
 import './header.scss';
 import NavLogo from '../../assets/logo/nav-logo.png';
 import { Link, NavLink, useLocation } from 'react-router-dom';
-import React from 'react';
+import React, { useState } from 'react';
 
 function Header(props) {
     const { pathname } = useLocation();
+    // const [setClick] = useState(false)
+    // const closeMenu = () => setClick(false)
+
     return (
         <header className="header">
             <Link to="/">
                 <img className="header__logo" src={NavLogo} alt="nav-logo" ></img></Link>
             <div className="header__nav" >
+                {/* <Link to="notes" className="header__logo--link" spy={true} smooth={true} offset={50} duration={500} onClick={closeMenu} >Notes</Link> */}
                 <NavLink to="/notes"
                     className="header__logo--link" activeClassName="header__logo--active" isActive={() => ["/notes"].includes(pathname)}>Notes</NavLink>
                 <NavLink to="/calendar"
--- src/components/notes/notes.js
+++ src/components/notes/notes.js
@@ -4,6 +4,9 @@ function Notes() {
     return (
         <div>
             <h1>notes</h1>
+            <p>adfadfasdfasdfasadfadfasdfasdfasdfadfadfadfasdfasdfasdfadfadfadfasdfasdfasdfadfadfadfasdfasdfasdfadfdfadf</p>
+            <p>adfadfasdfasdfasadfadfasdfasdfasdfadfadfadfasdfasdfasdfadfadfadfasdfasdfasdfadfadfadfasdfasdfasdfadfdfadf</p>
+
         </div>
     )
 }
