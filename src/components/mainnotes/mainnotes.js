e25fea981df28a3f4970210f121b557f8b024fbd
chelsia tong <chelsiatong@gmail.com>
2022-07-27 23:25:32 -0400

    need to style notes, but basically finished functionality

--- src/components/mainnotes/mainnotes.js
+++ src/components/mainnotes/mainnotes.js
@@ -1,16 +1,27 @@
 import './mainnotes.scss';
+import ReactMarkdown from "react-markdown";
 
 
-function MainNotes() {
+function MainNotes({ activeNote, onUpdateNote }) {
+    const onEditField = (key, value) => {
+        onUpdateNote({
+            ...activeNote,
+            [key]: value,
+            time: Date.now(),
+        });
+    };
+
+    if (!activeNote) return <div className="not-selected">Please select a note</div>;
+
     return (
         <section className="main-notes">
             <div className="main-notes--edit" >
-                <input className="main-notes--edit__title" type="text" id="title" placeholder="What do you want to name me?" autoFocus />
-                <textarea className="main-notes--edit__body" id="body" placeholder="What's on your mind?" />
+                <input className="main-notes--edit__title" type="text" id="title" placeholder="What do you want to name me?" value={activeNote.title} onChange={(event) => onEditField("title", event.target.value)} autoFocus />
+                <textarea className="main-notes--edit__body" id="body" placeholder="What's on your mind?" value={activeNote.body} onChange={(event) => onEditField("body", event.target.value)} />
             </div>
             <div className="main-notes__preview" >
-                <h3 className="main-notes__preview--title" >Title</h3>
-                <div className="main-notes__preview--body" >Note Preview</div>
+                <h3 className="main-notes__preview--title" >{activeNote.title}</h3>
+                <ReactMarkdown className="main-notes__preview--body" >{activeNote.body}</ReactMarkdown>
             </div>
 
         </section>
