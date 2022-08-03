c5303a83b7b5cfffc28ae1356718357116be4ab4
chelsia tong <chelsiatong@gmail.com>
2022-08-02 22:08:30 -0400

    adding styling to todolist

---
+++ src/components/ToDo/todocard/todocard.js
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import { withRouter } from "react-router-dom";
+import EditTask from '../todomodals/edittask/edittask';
+import './todocard.scss';
+
+function ToDoCard({ taskObj, index, deleteTask, updateListArr }) {
+    const [modal, setModal] = useState(false);
+
+    const toggle = () => {
+        setModal(!modal);
+    }
+
+    const updateTask = (object) => {
+        updateListArr(object, index);
+    }
+
+    const handleDelete = () => {
+        deleteTask(index);
+    }
+
+    return (
+        <section className="card">
+            <div className="card__top" ></div>
+            <div className="card__container">
+                <span className="card__head">{taskObj.Name}</span>
+                <p className="card__description">{taskObj.Description}</p>
+                <div>
+                    <button className="card__button" onClick={() => setModal(true)}>Edit</button>
+                    <button className="card__button" onClick={handleDelete} >Delete</button>
+                </div>
+            </div>
+            <EditTask modal={modal} toggle={toggle} updateTask={updateTask} taskObj={taskObj} />
+        </section>
+    )
+}
+
+export default ToDoCard;
---
+++ src/components/ToDo/todomodals/edittask/edittask.js
@@ -0,0 +1,54 @@
+import react, { useEffect, useState } from 'react';
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
+
+function EditTaskModal({ modal, toggle, updateTask, taskObj }) {
+    const [taskName, setTaskName] = useState('');
+    const [description, setDescription] = useState('');
+
+    const handleChange = (event) => {
+        const { name, value } = event.target;
+
+        if (name === "taskName") {
+            setTaskName(value);
+        } else {
+            setDescription(value);
+        }
+    }
+
+    const handleUpdate = (event) => {
+        event.preventDefault();
+        let taskObj = {};
+        taskObj["Name"] = taskName;
+        taskObj["Description"] = description;
+        updateTask(taskObj);
+    }
+
+    useEffect(() => {
+        setTaskName(taskObj.Name);
+        setDescription(taskObj.Description);
+    }, []);
+
+    return (
+        <Modal className="modal" isOpen={modal} toggle={toggle}>
+            <ModalHeader className="modal__header" toggle={toggle}>Update Task</ModalHeader>
+            <ModalBody>
+                <div className="modal__container">
+                    <div className="modal__head" >
+                        <label className="modal__title" >task name title</label>
+                        <input className="modal__text" type="text" value={taskName} onChange={handleChange} name="taskName"></input>
+                    </div>
+                    <div>
+                        <label className="modal__description" >Description</label>
+                        <textarea className="modal__text" value={description} onChange={handleChange} name="description"></textarea>
+                    </div>
+                </div>
+            </ModalBody>
+            <ModalFooter>
+                <Button className="modal__cancel" onClick={toggle} >cancel</Button>{''}
+                <Button className="modal__create" onClick={handleUpdate} >update</Button>
+            </ModalFooter>
+        </Modal>
+    );
+}
+
+export default EditTaskModal;
