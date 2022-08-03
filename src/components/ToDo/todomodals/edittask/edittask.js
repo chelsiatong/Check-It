879dd2fdbd02a833cf4443d7683666afd768d168
chelsia tong <chelsiatong@gmail.com>
2022-08-03 17:19:19 -0400

    cleaned up code and did styling

--- src/components/ToDo/todomodals/edittask/edittask.js
+++ src/components/ToDo/todomodals/edittask/edittask.js
@@ -35,11 +35,11 @@ function EditTaskModal({ modal, toggle, updateTask, taskObj }) {
                 <div>
                     <div className="modal__container">
                         <div className="modal__head" >
-                            <label className="modal__head--title" >task name title</label>
+                            <label className="modal__head--title" >Task</label>
                             <input className="modal__head--text" type="text" value={taskName} onChange={handleChange} name="taskName"></input>
                         </div>
                         <div className="modal__detail">
-                            <label className="modal__detail--description" >Description</label>
+                            <label className="modal__detail--title" >Description</label>
                             <textarea className="modal__detail--text" value={description} onChange={handleChange} name="description"></textarea>
                         </div>
                     </div>
