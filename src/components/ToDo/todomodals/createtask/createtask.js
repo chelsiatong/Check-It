09dd7e27dd3257ac0bd4a267babf366ff9e55497
chelsia tong <chelsiatong@gmail.com>
2022-08-03 17:39:31 -0400

    adding styling to todo

--- src/components/ToDo/todomodals/createtask/createtask.js
+++ src/components/ToDo/todomodals/createtask/createtask.js
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-// import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 import './createtask.scss';
 
 function CreateTaskModal({ modal, toggle, save }) {
@@ -48,29 +47,6 @@ function CreateTaskModal({ modal, toggle, save }) {
         </section>
     );
 
-    // return (
-    //     <section className="modal">
-    //         <Modal className="modal__section" isOpen={modal} toggle={toggle}>
-    //             <ModalHeader className="modal__header" toggle={toggle}>Create Task</ModalHeader>
-    //             <ModalBody>
-    //                 <div className="modal__container">
-    //                     <div className="modal__head" >
-    //                         <label className="modal__title" >Task</label>
-    //                         <input className="modal__text" type="text" value={taskName} onChange={handleChange} name="taskName"></input>
-    //                     </div>
-    //                     <div>
-    //                         <label className="modal__description" >Description</label>
-    //                         <textarea className="modal__text" value={description} onChange={handleChange} name="description"></textarea>
-    //                     </div>
-    //                 </div>
-    //             </ModalBody>
-    //             <ModalFooter>
-    //                 <Button className="create-modal__cancel" onClick={toggle} >Cancel</Button>
-    //                 <Button className="create-modal__create" onClick={handleSave} >Create</Button>
-    //             </ModalFooter>
-    //         </Modal>
-    //     </section>
-    // );
 }
 
 export default CreateTaskModal;
