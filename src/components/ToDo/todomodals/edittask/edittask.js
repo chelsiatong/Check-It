e05c1d6dfbb4f1a42cb980e7c44e456d075b55cc
chelsia tong <chelsiatong@gmail.com>
2022-08-03 17:17:49 -0400

    cleaned up code

--- src/components/ToDo/todomodals/edittask/edittask.js
+++ src/components/ToDo/todomodals/edittask/edittask.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react';
-// import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 import '../createtask/createtask.scss';
 
 function EditTaskModal({ modal, toggle, updateTask, taskObj }) {
@@ -52,32 +51,6 @@ function EditTaskModal({ modal, toggle, updateTask, taskObj }) {
             </div>
         </section>
     );
-
-    // return (
-    //     <section className="modal">
-    //         <Modal className="modal__section" >
-    //             <ModalHeader className="modal__header" >Update Task</ModalHeader>
-    //             <ModalBody>
-    //                 <div className="modal__container">
-    //                     <div className="modal__head" >
-    //                         <label className="modal__title" >task name title</label>
-    //                         <input className="modal__text" type="text" value={taskName} onChange={handleChange} name="taskName"></input>
-    //                     </div>
-    //                     <div>
-    //                         <label className="modal__description" >Description</label>
-    //                         <textarea className="modal__text" value={description} onChange={handleChange} name="description"></textarea>
-    //                     </div>
-    //                 </div>
-    //             </ModalBody>
-    //             <ModalFooter>
-    //                 <Button className="modal__cancel" onClick={toggle} >cancel</Button>
-    //                 <Button className="modal__create" onClick={handleUpdate} >update</Button>
-    //             </ModalFooter>
-    //         </Modal>
-    //     </section>
-    // );
-
-
 
 }
 
